df0b3cfdcb58f7c9f5bfbe5ac0cd7c9fc09b868b
EEngvall <erikengvall@outlook.com>
2022-05-10 21:16:21 -0700

    Completed UserInfo class. Added MD5 hash function.

--- Main.py
+++ Main.py
@@ -1,32 +1,24 @@
 from pprint import pprint
+import json
+import getpass
 
 
-credentialFile = open("credentialsfile.txt", 'r')
-inputFile = credentialFile.readlines()
+from UserInfo import userInfo
 
-fileCheck = "griffin.kees"
+credentialFile = "userCredentials.json"
 numAttempts = 3
 
-
-def validateUserName(userName):
-    global numAttempts
-    isFound = False
-    for line in inputFile:
-        if userName in line:
-            isFound = True
-            break
-    return isFound
+# Function to track and display attempt information
 
 
-def validatePassword(userPassword):
-    if userPassword == "password":
-        print("Correct")
+def checkNumAttempts():
+    if numAttempts > 0:
+        print("{} attempts remaining".format(numAttempts))
+    else:
+        print("Exceeded maximum number of attempts, system will now exit")
+        exitClient()
 
-
-def consumeAttempt():
-    global numAttempts
-    numAttempts -= 1
-    print("Username does not exist, please try again, you have {} attempts remaining".format(numAttempts))
+# Simple function to exit with message.
 
 
 def exitClient():
@@ -34,15 +26,26 @@ def exitClient():
     quit()
 
 
-while (numAttempts > 0):
-    userName = input("Enter User Name or Q to exit: ")
-    if userName == "Q" or userName == "q":
-        exitClient()
-    else:
-        if validateUserName(userName):
-            userPassword = input("Enter password: ")
-            validatePassword(userPassword)
+print("Please enter a selection")
+print("1. Login")
+print("2. Exit")
+userSelection = input("")
+if userSelection == "1":
+    # Attempts this loop while there are still attempts remaining.
+    while numAttempts > 0:
+        userName = input("Enter User Name: ")
+        # create new instance of getpass in order to conceal password input echoing.
+        userPassword = getpass.getpass("Enter password: ")
+        # Creates new instance of UserInfo class passing the username, password, and default credential file as arguments
+        validatedSystemUser = userInfo(
+            userName, userPassword, credentialFile)
+        # Checks the return of the validateUser function, and if true displays role file, otherwise reduces
+        # the number of attempts and re enters the loop for an additional attempt.
+        if validatedSystemUser.validateUser():
+            validatedSystemUser.displayRoleFile()
+            break
         else:
-            consumeAttempt()
-
-print("Too many failed attempts, exiting")
+            numAttempts -= 1
+            checkNumAttempts()
+else:
+    exitClient()
--- UserInfo.py
+++ UserInfo.py
@@ -1,10 +1,23 @@
 from pprint import pprint
+import json
+import hashlib
 
 
 class userInfo:
-
     userRole = ""
     userCredentialStr = ""
+    hashedPassword = ""
+
+    def __init__(self, userName, userPassword, credentialFile):
+        self.userName = userName
+        self.userPassword = userPassword
+        self.credentialFile = credentialFile
+
+    def getUserName(self):
+        return self.userName
+
+    def getUserPassword(self):
+        return self.userPassword
 
     def getUserRole(self):
         return self.userRole
@@ -12,8 +25,35 @@ class userInfo:
     def setUserRole(self, role):
         self.userRole = role
 
-    def getUserCredentialStr(self):
-        return self.userCredentialStr
-
     def setUserCredentialStr(self, credentialStr):
         self.userCredentialStr = credentialStr
+
+    def createPasswordHash(self):
+        result = hashlib.md5(self.userPassword.encode())
+        self.hashedPassword = result.hexdigest()
+
+    def validateUser(self):
+        userValidated = False
+        with open(self.credentialFile, 'r') as dataFile:
+            data = json.load(dataFile)
+            # Iterate through the JSON file searching for the matching Key:Values
+            for key in data["credentials"]:
+                if self.userName == key["username"]:
+                    # Apply MD5 hash to user input for password value.
+                    self.createPasswordHash()
+                    print(self.hashedPassword)
+                    if self.hashedPassword == key["passwordHash"]:
+                        userValidated = True
+                        self.userRole = key["role"]
+                    else:
+                        print("Incorrect Password")
+        return userValidated
+        # print(key["username"])
+
+    # Prints contents of user role file to screen
+    def displayRoleFile(self):
+        print()
+        with open(self.userRole + ".txt", "r") as file:
+            for line in file:
+                print(line)
+        print()
